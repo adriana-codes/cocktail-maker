f483d36a26cf640e95fe33489dbf49b3bb89e2fa
adriana-codes <helloadrianadiaz@gmail.com>
2024-10-27 18:55:01 +0100

    +css +js

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ function displayCocktail(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
+    delay: "natural",
   });
 }
 
@@ -13,9 +14,12 @@ function generateCocktail(event) {
   let apiKey = "0be192793f55aa475o5602t2cabd6e24";
   let prompt = `User Instructions: Generate a cocktail with the ingredient ${instructionsInput.value}`;
   let context =
-    "You are an experienced mixologist who crafts special cocktials. Your mission is to only generate a cocktail based on one ingredient in basic HTML following the user instructions in a <p> and separate each line with <br />.";
+    "You are an experienced mixologist who crafts special cocktails. Your mission is to only generate a cocktail based on the ingredients in basic HTML following the user instructions in a <p> and separate each line with <br />. List the ingredients and then explain the recipe. Finish the recipe with <em>Cheers! 🥂</em>";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let cocktailMakerElement = document.querySelector("#cocktail-maker");
+  cocktailMakerElement.classList.remove("hide");
+  cocktailMakerElement.innerHTML = `<div class="preparing"> 🍸 Shaking a cocktail for you with <em>${instructionsInput.value}</em></div>`;
   axios.get(apiUrl).then(displayCocktail);
 }
 
